e7c2cfe32277cd4f0a5788bae45e7e14a4da86f7
Juan Blandini <juanboca@gmail.com>
2013-03-18 12:41:50 -0300

    Removing typecast to symbol

--- lib/enum/enum_adapter.rb
+++ lib/enum/enum_adapter.rb
@@ -17,50 +17,6 @@ end
 
 column_class.module_eval do
 
-  alias __klass_enum klass
-  # The class for enum is Symbol.
-  def klass
-    if type == :enum
-      Symbol
-    else
-      __klass_enum
-    end
-  end
-
-  alias __type_cast_enum type_cast
-  # Convert to a symbol.
-  def type_cast(value)
-    if type == :enum
-      self.class.value_to_symbol(value)
-    else
-      __type_cast_enum(value)
-    end
-  end
-
-  alias __type_cast_code_enum type_cast_code
-  # Code to convert to a symbol.
-  def type_cast_code(var_name)
-    if type == :enum
-      "#{self.class.name}.value_to_symbol(#{var_name})"
-    else
-      __type_cast_code_enum(var_name)
-    end
-  end
-
-  class << self
-    # Safely convert the value to a symbol.
-    def value_to_symbol(value)
-      case value
-      when Symbol
-        value
-      when String
-        value.empty? ? nil : value.intern
-      else
-        nil
-      end
-    end
-  end
-
 private
   alias __simplified_type_enum simplified_type
   # The enum simple type.
@@ -71,7 +27,7 @@ private
       __simplified_type_enum(field_type)
     end
   end
-  
+
   alias __extract_limit_enum extract_limit
   def extract_limit(sql_type)
     if sql_type =~ /^enum/i
@@ -80,6 +36,5 @@ private
       __extract_limit_enum(sql_type)
     end
   end
-
 
 end
